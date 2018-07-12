fb16f6cd059742a5b2a0572a23884b8a5f056925
Frederic Vogels <frederic.vogels@gmail.com>
2018-07-12 10:31:04 +0200

    assertion index

--- src/assertions/index.ts
+++ src/assertions/index.ts
@@ -1,6 +1,7 @@
 export { IResult } from './result';
 export { IAssertion } from './assertion';
 export { equality } from './equality';
+export { approximately } from './approximately';
 export { noReturn } from './no-return';
 export { permutation } from './permutation';
 export { unmodified } from './unmodified';
