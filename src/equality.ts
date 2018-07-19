b18e4591b832bed7838ffe2faef5b21f882afac2
Frederic Vogels <frederic.vogels@gmail.com>
2018-07-19 20:24:35 +0200

    added approximately equality

--- src/equality.ts
+++ src/equality.ts
@@ -5,3 +5,8 @@ export function deepEqual(x : any, y : any) : boolean
 {
     return isEqual(x, y);
 }
+
+export function approximately(x : number, y : number, epsilon : number = 0.0001) : boolean
+{
+    return Math.abs(x - y) < epsilon;
+}
