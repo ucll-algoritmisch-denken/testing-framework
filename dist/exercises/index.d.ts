01f36de255c8ee9365e73602a8ee88bbcb4bdb88
Frederic Vogels <frederic.vogels@gmail.com>
2018-08-21 20:29:04 +0200

    exercises

--- dist/exercises/index.d.ts
+++ dist/exercises/index.d.ts
@@ -2,7 +2,7 @@ export * from './exercise';
 import { ITestCase as _ITestCase } from './test-case';
 import { CollapsibleTestCase as _CollapsibleTestCase } from './collapsible-test-case';
 import { TestCaseBasedCodingExercise } from './test-case-based-coding-exercise';
-import { ReferenceImplementationBasedCodingExercise } from './reference-implementation-based-coding-exercise';
+import { ReferenceImplementationBasedCodingExercise, ITestCaseInput as _ITestCaseInput } from './reference-implementation-based-coding-exercise';
 import { ReturnValueCodingExercise } from './return-value-coding-exercise';
 export declare namespace Coding {
     const Exercise: typeof TestCaseBasedCodingExercise;
@@ -10,6 +10,7 @@ export declare namespace Coding {
         type ITestCase = _ITestCase;
         const CollapsibleTestCase: typeof _CollapsibleTestCase;
         namespace ReferenceBased {
+            type ITestCaseInput<Ps extends any[], R> = _ITestCaseInput<Ps, R>;
             const Exercise: typeof ReferenceImplementationBasedCodingExercise;
             namespace ReturnValue {
                 const Exercise: typeof ReturnValueCodingExercise;
