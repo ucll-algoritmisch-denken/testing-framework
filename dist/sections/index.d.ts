a2c64290ba0d74bb4eac76308264747839661630
Frederic Vogels <frederic.vogels@ucll.be>
2018-10-26 22:53:14 +0200

    fixed #4

--- dist/sections/index.d.ts
+++ dist/sections/index.d.ts
@@ -2,3 +2,4 @@ export * from './finish-line-section';
 export * from './explanations';
 export * from './reference-based-exercise-section';
 export * from './exercise-section';
+export * from './coding-exercise';
